2cb06990cc4af538c838f1ab6f120265f7782470
Renan Loes <renanloes8@gmail.com>
2024-03-06 12:15:22 -0300

    correcao do js

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 let input = document.querySelectorAll('.input');
+let button = document.querySelector('.button')
 
 input.forEach(function(item){
     item.addEventListener('input', () =>{
+        console.log("ta funcional")
 
-        if(input.length > 0){
+        if(item.value !== ""){
             item.classList.add('preenchido');
             item.classList.remove('vazio');
-
-        }else if(input.length == 0){
+        }else{
             item.classList.add('vazio');
             item.classList.remove('preenchido');
         }
     });
+
+    button.addEventListener('click', () => {
+        if(item.value !== ""){
+            item.classList.add('preenchido');
+            item.classList.remove('vazio');
+        }else{
+            item.classList.add('vazio');
+            item.classList.remove('preenchido');
+        }
+    })
 });
